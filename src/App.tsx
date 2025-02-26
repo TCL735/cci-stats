c7efc90c229423c5b614ba18d09b7d98cbbe728f
Timmy Luong <TCL735@users.noreply.github.com>
2025-02-26 13:58:19 -0800

    feat: update data through 2025-02-24

--- src/App.tsx
+++ src/App.tsx
@@ -138,7 +138,7 @@ const Stats2025 = () => (
   <Table2025Context.Provider value={TenTon2025}>
     <StatsTracker
       label={`Ten Ton is Number 1's Win/Loss`}
-      endLabelOffset={[-85, 0]}
+      endLabelOffset={[-50, -15]}
     />
   </Table2025Context.Provider>
 );
